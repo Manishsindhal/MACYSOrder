cb38d2f1a7fe2a297044fd6ffa4ccff2dbce8ad6
manish <sindhalmanish@gmail.com>
2022-01-19 13:29:34 +0530

    Change is pom file

--- src/test/java/com/macys/macysorder/MACYSOrderControllerTest.java
+++ src/test/java/com/macys/macysorder/MACYSOrderControllerTest.java
@@ -6,7 +6,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,26 +15,34 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.macys.macysorder.dto.UpdateOrderStatusRequest;
 import com.macys.macysorder.service.UpdateOrderService;
 
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
 
-
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 //@WebMvcTest(OrderController.class)
+@ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 @SpringBootTest
-class MACYSOrderControllerTest {
+public class MACYSOrderControllerTest {
 	
 	@MockBean
     UpdateOrderService orderMessageService;
 	
 	@Autowired
     private MockMvc mockMvc;
+	
+	public MACYSOrderControllerTest() {
+		
+	}
 
 	@Test
     void testUpdateOrderStatus() throws Exception {
